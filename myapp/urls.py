236c97018d6221faf1b6337ccee744abc5c58411
Susan Tyler <Susan Tyler>
2016-08-03 22:58:18 -0400

    Updated main template and routing

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.user_list, name='user_list'),
-    url(r'^user/new/$', views.add_user, name='add_user'),
+    url(r'^$', views.home, name="home"),
+    url(r'^user/new/$', views.add_user, name="add_user"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from .models import User
 from .forms import UserForm
 
+def home(request):
+    return render(request, 'home.html', {})
+
 def user_list(request):
     users = User.objects.all()
-    form = UserForm()
-    return render(request, 'user_list.html', {'users':users, 'form':form})
+    return render(request, 'user_list.html', {'users':users})
+
 
 def add_user(request):
     if request.method == "POST":
         form = UserForm(request.POST)
-        form.save()
-    users = User.objects.all()
-    form = UserForm()
-    return render(request, 'user_list.html', {'users':users, 'form':form})
+        user = form.save()
+        return render(request, 'user_thanks.html', {'user':user})
+    else:
+        form = UserForm()
+        return render(request, 'add_user.html', {'form':form})
